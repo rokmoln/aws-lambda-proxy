b346a80db9e54a4cb882f438e40b7eaaa2a34ebb
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2016-09-13 23:19:46 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-/* eslint-disable */
 try {
   require('babel-register'); // eslint-disable-line global-require
 } catch (_e) {
-  //
+  // assume already transpiled
 }
-
-// BEGIN -- APEX original
-try {
-  var config = require('./.env.json')
-  for (var key in config) {
-    process.env[key] = config[key]
-  }
-} catch (err) {
-  // ignore
-}
-// END -- APEX original
 
 module.exports = require('./src/index');
