674c409f0ab3b358628a0f3e7ac7b3fc3c81691e
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2017-02-01 23:51:39 +0100

    clone stageVariables

--- src/express-app.js
+++ src/express-app.js
@@ -150,10 +150,10 @@ export let loadLambdas = function({app, lambdas, stageVariables}) {
 
     if (apiSecondaryBasePath) {
       router = apiSecondaryRouters[apiSecondaryBasePath];
-      _.merge(stageVariables, {
-        API_SECONDARY_BASE_URL: `${stageVariables.API_BASE_URL}/${apiSecondaryBasePath}`,
+      stageVariables = _.defaults({
+        API_SECONDARY_BASE_URL: `${stageVariables.API_BASE_URL}${apiSecondaryBasePath}`,
         API_SECONDARY_BASE_PATH: apiSecondaryBasePath
-      });
+      }, stageVariables);
       if (!router) {
         router = new express.Router();
         apiSecondaryRouters[apiSecondaryBasePath] = router;
